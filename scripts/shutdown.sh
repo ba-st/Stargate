6814ca4ecc8aa22da8029c89e9f1cf4942dbd4e0
Gabriel Omar Cotelli <g.cotelli@gmail.com>
2019-12-06 13:44:58 -0300

    Fix tabulation

--- scripts/shutdown.sh
+++ scripts/shutdown.sh
@@ -3,8 +3,8 @@
 source ".operational-plugin.sh"
 
 curl --fail --request POST \
-	  --header "Authorization: Bearer $JWT" \
-		--header "Content-Type:application/json" \
-	  --header "Accept: application/json" \
-		--data '{"jsonrpc": "2.0" ,"method": "shutdown"}' \
-	  "${BASE_URL}":"${PORT}"/operations/application-control
+	--header "Authorization: Bearer $JWT" \
+	--header "Content-Type:application/json" \
+	--header "Accept: application/json" \
+	--data '{"jsonrpc": "2.0" ,"method": "shutdown"}' \
+	"${BASE_URL}":"${PORT}"/operations/application-control
